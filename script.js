6115f2ee3098d86aaf763ed809c79d037f216bb4
Roberto Santiago <delariva31.rss@gmail.com>
2024-11-12 16:27:14 +0000

    edit index.html , styles.css and script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
-
+// script.js
+document.addEventListener("DOMContentLoaded", function () {
+    const navbar = document.querySelector(".navbar");
+    const hamburger = document.querySelector(".hamburger");
+  
+    hamburger.addEventListener("click", () => {
+      navbar.classList.toggle("active");
+    });
+  });
+  
